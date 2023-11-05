2b7b87cbc951d3b20b02c4c6e391618f7263ebfe
Johnne Geograpo <59752069+MyLastStack@users.noreply.github.com>
2023-11-05 00:53:22 -0600

    updated

--- PlayerFeedback/Assets/Scripts/DoorAnimationTrigger.cs
+++ PlayerFeedback/Assets/Scripts/DoorAnimationTrigger.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class DoorAnimationTrigger : MonoBehaviour
 {
+    [SerializeField] Animation animator;
+    public bool playing;
     void Start()
     {
-        
+        playing = false;
     }
 
     void Update()
     {
-        
+        if (playing)
+        {
+            animator.Play();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playing = true;
+        }
     }
 }
